5099799ad81bf48cda9a969caf4625f5b987606a
Raddy <rx3mer@live.co.uk>
2020-09-18 15:42:08 +0100

    Final Recording

--- app.js
+++ app.js
@@ -7,21 +7,20 @@ const port = 5000
 // Static Files
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/js', express.static(__dirname + 'public/js'))
 app.use('/img', express.static(__dirname + 'public/img'))
+app.use('/js', express.static(__dirname + 'public/js'))
 
-// Templating Language
-app.set('views', './src/views');
-app.set('view engine', 'ejs');
+// Templating Engine
+app.set('views', './src/views')
+app.set('view engine', 'ejs')
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended : true }))
 
 // Routes
 const newsRouter = require('./src/routes/news')
-// const newsRouterSingle = require('./src/routes/newsSingle')
- 
+
 app.use('/', newsRouter)
 app.use('/article', newsRouter)
 
-// Listen on port
+// Listen on port 5000
 app.listen(port, () => console.log(`Listening on port ${port}`))
