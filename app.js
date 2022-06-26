335cb5ccbd3d5fc3438e4ee4f5f2e81d5413f6f5
Raddy <rx3mer@live.co.uk>
2022-06-26 13:00:58 +0100

    make it better

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
-const port = 3000
+const port = 5000
 
 // Static Files
 app.use(express.static('public'))
